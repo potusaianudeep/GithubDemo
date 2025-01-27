9944898dc0da7700d2f6b6fc3a2342254a4d59e3
potusaianudeep <123322861+potusaianudeep@users.noreply.github.com>
2025-01-27 13:59:32 -0700

    Add files via upload
    Object Creation

---
+++ Object.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Object {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(10);
+        l.add(20);
+        l.add(10);
+        l.add(null);
+        l.add(2,90);
+        System.out.println(l
+        );
+
+    }
+}
