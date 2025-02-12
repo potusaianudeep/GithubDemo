0dc1291e4ddf796a9dcc21f3ccb1659aa68b00bf
potusaianudeep <123322861+potusaianudeep@users.noreply.github.com>
2025-02-11 20:34:56 -0700

    Add files via upload
    Loops (While Loop)

---
+++ WhileLoop.java
@@ -0,0 +1,21 @@
+package ConditionalStatementsLoops;
+
+//Display even numbers between 1 to 10 numbers.
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i<=10)
+        {
+
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
