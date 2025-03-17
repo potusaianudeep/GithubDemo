e18db1ed4048c11f0195b206e06521b258f72a9c
potusaianudeep <123322861+potusaianudeep@users.noreply.github.com>
2025-03-16 18:26:56 -0600

    Add files via upload
    Enhanced For Loop with Array.

---
+++ ArrayForLoop.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+//Interating array with enchanced for loop....
+
+public class ArrayForLoop {
+
+    public static void main(String[] args) {
+
+        int [] a = {10,12,13,14};
+
+        for (int temp:a)   //Here a variable is the source data.
+        {
+            System.out.println(temp);
+        }
+
+
+    }
+}
